bfef5baec99fb8898d82b63116bd6dceb69de595
Dmitry <fendermany@gmail.com>
2021-03-16 20:54:07 +0200

    60%

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const hamburger = document.querySelector('.hamburger'),
       menu = document.querySelector('.menu'),
       closeElem = document.querySelector('.menu__close');
@@ -9,3 +11,32 @@ hamburger.addEventListener('click', () => {
 closeElem.addEventListener('click', () => {
     menu.classList.remove('active');
 });
+
+$('.carousel').carousel({
+  interval: 4000
+});
+
+
+
+
+// alert ('Hello');
+
+
+// const result = confirm("Are you here?");
+
+// console.log(result);
+
+// const answer = prompt("Вам есть 18?", "18");
+// console.log(answer + 5);
+
+
+// const answers = [];
+
+// answers[0] = prompt('Как ваше имя?', '');
+// answers[1] = prompt('Как ваша фамилия?', '');
+// answers[2] = prompt('Сколько вам лет?', '');
+
+// console.log(typeof(answers));
+
+
+// console.log(typeof(null));
